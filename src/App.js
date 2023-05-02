f30da763db707e14e92e6737172c421e3f6bf0c2
Alexandre Leroux <alex@sherpa.one>
2023-05-02 17:05:18 +0200

    switcher ok

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import FruitsMaster from "./components/FruitsMaster";
+import react, { useState } from "react";
 
 function App() {
+
+  const [displayList, setDisplayList] = useState(false);
+  //par défaut displayList a la valeur false
+  //donc on cache la liste
+
+  function onClick() {
+    setDisplayList(displayList ? false : true);
+    //condition ternaire
+  }
+
   return (
     <div className="App">
-      <FruitsMaster />
+      <button onClick={()=>onClick()}>Afficher / Masquer</button>
+      
+      {/* {displayList && <FruitsMaster />}
+      {!displayList && <p>Pas de fruit</p>} */}
+
+      {displayList ? <FruitsMaster/> : <p>Pas de fruit</p> }
+
     </div>
   );
 }
