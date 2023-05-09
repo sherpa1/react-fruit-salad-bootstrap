d0c039fbcc0b5bb683bb21a4dc083e083b05cbb7
Alexandre Leroux <alex@sherpa.one>
2023-05-09 08:58:38 +0200

    reload button + UI

--- src/components/FruitsMaster.js
+++ src/components/FruitsMaster.js
@@ -4,11 +4,13 @@ import { v4 as uuid } from "uuid";
 import FruitPreview from "./FruitPreview";
 import Fruit from "../models/Fruit";
 
+import "./FruitsMaster.css";
+
 import axios from "axios";
 
-const axiosIntance = axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://fruits.shrp.dev",
-  timeout: 1000,
+  timeout: 3000,
   headers: {},
 });
 
@@ -16,12 +18,17 @@ function FruitsMaster() {
   const [fruits, setFruits] = useState([]); //par défaut la liste de fruits est vide
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [needToReload, setNeedToReload] = useState(false);
+
+  function onLoadData() {
+    setNeedToReload(true); //déclenche l'exécution de useEffect
+  }
 
   useEffect(() => {
     async function fetchFruitsFromAPI() {
       try {
         setLoading(true);
-        const response = await axiosIntance.get("/items/fruits");
+        const response = await axiosInstance.get("/items/fruits");
         const collectionOfFruitItems = response.data.data.map(
           (jsonItem) => new Fruit(jsonItem.name, jsonItem.color, jsonItem.image)
         );
@@ -36,13 +43,16 @@ function FruitsMaster() {
     }
 
     fetchFruitsFromAPI();
-  }, []);
+  }, [needToReload]);
+  //on indique que useEffect a une dépendance à needToReload
+  //-> si needToReload évolue, useEffect doit être appelé
 
   return (
     <div className="FruitsMaster">
-      <div>
-        {loading === true && <p>Chargement...</p>}
-        {error === true && <p>Une erreur s'est produite :(</p>}
+      <button onClick={() => onLoadData()}>Recharger les données</button>
+      {loading === true && <p>Chargement...</p>}
+      {error === true && <p>Une erreur s'est produite</p>}
+      <div className="FruitsContainer">
         {fruits.map((fruit) => (
           <FruitPreview key={uuid()} fruit={fruit} />
         ))}
